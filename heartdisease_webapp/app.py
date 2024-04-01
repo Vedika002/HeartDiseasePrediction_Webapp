80d78cdda2612ebf449b8d28dc63056188176cbf
Vedika Chauhan <vedika.2024cs1135@kiet.edu>
2024-04-02 03:01:57 +0530

    modified app.py

--- heartdisease_webapp/app.py
+++ heartdisease_webapp/app.py
@@ -1,5 +1,5 @@
 import numpy as np
-from flask import Flask, request, render_template
+from flask import Flask,redirect,url_for, request, render_template
 import pickle
 
 app= Flask(__name__)
@@ -7,6 +7,10 @@ app= Flask(__name__)
 model= pickle.load(open('model.pkl','rb'))
 
 @app.route('/')
+def getstarted():
+    return render_template("getstart.html")
+
+@app.route('/home')
 def home():
     return render_template('ind.html')
 
@@ -17,11 +21,19 @@ def predict():
     features = np.array(int_features)
     reshaped=features.reshape(1,-1)
     prediction = model.predict(reshaped)
-    if prediction == 0:
+    if prediction == 0: 
         predicts=0
     else:
         predicts=1    
-    return render_template('ind.html', prediction=predicts)
+    # return render_template('result.html', prediction=predicts)
+    return redirect(url_for('load', prediction=predicts))
+    # return redirect(url_for('result', prediction=predicts))
+@app.route('/load/<int:prediction>')
+def load(prediction):
+    return render_template('result.html',prediction=prediction)
+@app.route('/display_result/<int:prediction>')
+def display_result(prediction):
+    return render_template('display_result.html', prediction=prediction)
 
 if __name__ == "__main__":
     app.run()
